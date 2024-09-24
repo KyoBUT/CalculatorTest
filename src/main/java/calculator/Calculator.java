0b66b9defcdf25ba388d70a35477c1f065ad9424
Alex Moreno <alex.moreno32390@gmail.com>
2024-09-24 17:18:56 +0200

    First 3 tests

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,7 @@
 package calculator;
 
 public class Calculator {
+
+    public int add(int opG, int opD) {return opG + opD;}
+    public int divide(int opG, int opD) {return opG/opD;}
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,7 +1,54 @@
 package calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 
 class CalculatorTest {
 
+        @Test
+        void devrait_obtenir_le_resultat_de_laddition() {
+            //GIVEN
+            Calculator calculator = new Calculator();
+            int opG = 2;
+            int opD = 3;
+
+            //WHEN
+            int resultat = calculator.add(opG, opD);
+
+            //THEN
+            assertEquals(5, resultat);
+        }
+
+
+        @Test
+        void devrait_obtenir_le_resultat_de_la_division() {
+            //GIVEN
+            Calculator calculator = new Calculator();
+            int opG = 6;
+            int opD = 3;
+
+            //WHEN
+            int resultat = calculator.divide(opG, opD);
+
+            //THEN
+            assertEquals(2, resultat);
+        }
+
+        @Test
+        void devrait_obtenir_le_resultat_de_la_division_par_zero() {
+            //GIVEN
+            Calculator calculator = new Calculator();
+            int opG = 6;
+            int opD = 0;
+
+
+            try {
+                //WHEN
+                calculator.divide(opG, opD);
+            } catch (ArithmeticException e) {
+                //THEN
+                assertEquals("/ by zero", e.getMessage());
+            }
+        }
 }
